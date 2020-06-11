f89bee64273f13b2f71e0782934e314d62ee901c
jacobschenberg <66764618+jacobschenberg@users.noreply.github.com>
2020-06-11 14:26:55 -0700

    Add files via upload

---
+++ scripts/my_script.py
@@ -0,0 +1,21 @@
+"""Script to run my project."""
+
+# Imports
+from my_module.functions import *
+import random
+
+# PYTHON SCRIPT HERE
+
+# Global variables
+yes = ['A', 'a']
+no = ['B', 'b']
+quit = ['quit', 'Quit', 'Q', 'q']
+alternate_response = ["I'm sorry, could you try again?", 
+                      "One more time, please?", "I couldn't quite get that."]
+exit = ["Bye!", "See ya!", "Sayonara!", "So long!", "That was fun!"]
+
+
+# Calls "star_wars" function to start the whole program since it begins with this
+star_wars()
+
+# This is all I put for the script since it's really simple to follow as you progress throughout the code as all I have are a bunch of original functions that call upon one another in sequence
